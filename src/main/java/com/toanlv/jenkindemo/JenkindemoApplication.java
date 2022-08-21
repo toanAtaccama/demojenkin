bafbd2ea245062c05278dfb407a6e40e0983fa81
Toan Le <toan.le@ataccama.com>
2022-08-21 08:44:32 +0700

    first commit

---
+++ src/main/java/com/toanlv/jenkindemo/JenkindemoApplication.java
@@ -0,0 +1,13 @@
+package com.toanlv.jenkindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toanlv/jenkindemo/JenkindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toanlv.jenkindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
